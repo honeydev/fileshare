dfa5b4228dc1b3b40b78c0c1d6eeab9cd9d966e3
honey <devspades@gmail.com>
2017-07-28 17:56:09 +1000

    add slim skeleton

---
+++ config/cfg.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+    'displayErrorDetails' => true,
+    'addContentLengthHeader' => false,
+    'db' => array(
+        'host' => 'localhost',
+        'user' => 'honey',
+        'pass' => 'd5d7',
+        'dbname' => 'fileshare'
+    ));
