a0e04ee4e753c357c2cca9bc17c6bf3c860f6bfb
Alexey Lebedev <devspades@gmail.com>
2017-10-05 23:45:48 +1000

    change gitignore

---
+++ tests/unit/LoginValidatorTest.php
@@ -0,0 +1,54 @@
+<?php
+
+use Fileshare\Exceptions\FileshareException as FileshareException;
+
+class LoginValidatorTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+    private $loginValidator;
+
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testSomeFeature()
+    {
+        $this->loginValidator = new \Fileshare\Validators\LoginValidator();
+       // $this->testValidLogins();
+        $this->testInvalidLogins();
+    }
+
+    private function testValidLogins()
+    {
+        define('VALIDE_LOGINS', [
+           'user', 'name', 'alexey', 'masdadasd', 'asdasdas',
+           'ASDASDA', 'a6', 'g4', 'e666', 'Y7a423A1', 'user-name',
+            'user_name', 'honey.white'
+        ]);
+        for ($i = 0; $i < count(VALIDE_LOGINS); $i++) {
+            $this->tester->assertTrue($this->loginValidator->validate(VALIDE_LOGINS[$i]));
+        }
+    }
+    private function testInvalidLogins()
+    {
+        define('INVALID_LOGINS', [
+            '', ' ', ' SSDS',
+            'user name', '-user', '_user',
+            'sddddddddddddddddddddddddddddddddd', 'asda@!%^'
+        ]);
+        for ($i = 0; $i < count(INVALID_LOGINS); $i++) {
+            $invalidLogin = INVALID_LOGINS[$i];
+            $this->tester->expectException('FileshareException', function () use ($invalidLogin) {
+                $this->loginValidator->validate('');
+            });
+        }
+    }
+}
